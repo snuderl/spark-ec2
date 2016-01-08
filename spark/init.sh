ca829a324e97ee34126e768e07605aebc3ed782c
Blaž Šnuderl <snuderl@gmail.com>
2016-01-08 09:12:46 +0100

    Upgrade to hadoop 2.7.1

--- spark/init.sh
+++ spark/init.sh
@@ -2,8 +2,6 @@
 
 pushd /root > /dev/null
 
-echo "blaz fork"
-
 if [ -d "spark" ]; then
   echo "Spark seems to be installed. Exiting."
   return
@@ -126,13 +124,13 @@ else
         wget http://s3.amazonaws.com/spark-related-packages/spark-1.2.1-bin-hadoop2.4.tgz
       fi
       ;;
-    1.5.1)
+    *)
       if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
         wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-hadoop1.tgz
       elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
         wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-cdh4.tgz
       else
-        wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-hadoop2.4.tgz
+        wget http://s3.amazonaws.com/spark-related-packages/spark-$SPARK_VERSION-bin-hadoop2.7.1.tgz
       fi
       if [ $? != 0 ]; then
         echo "ERROR: Unknown Spark version"
